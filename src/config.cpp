1acf59a08ae2b9ad912929ebe556e16514bdc460
Andrej Babic <andrej.babic@psi.ch>
2018-01-10 15:48:23 +0100

    Config refactoring

--- src/config.cpp
+++ src/config.cpp
@@ -1,15 +1,17 @@
 #include "config.hpp"
 
 namespace config {
-    H5::PredType dataset_type = H5::PredType::NATIVE_UINT8;
-    H5T_order_t dataset_byte_order = H5T_ORDER_LE;
+    // Receiver config.
+    int n_io_threads = 1;
+
+    // Ring buffer config.
+    size_t ring_buffer_n_slots = 100;
+    uint32_t ring_buffer_read_retry_interval = 5;
+    
+    // H5 config.
+    std::string dataset_name = "data";
     hsize_t dataset_increase_step = 1000;
     hsize_t initial_dataset_size = 1000;
-
-    uint16_t rest_port = 11011;
-    int n_io_threads = 1;
-    size_t n_slots = 100;
-
-    std::string dataset_name = "data";
-
+    H5::PredType dataset_type = H5::PredType::NATIVE_UINT8;
+    H5T_order_t dataset_byte_order = H5T_ORDER_LE;
 }
--- src/config.hpp
+++ src/config.hpp
@@ -6,16 +6,16 @@
 
 namespace config
 {
-    extern H5::PredType dataset_type;
-    extern H5T_order_t dataset_byte_order;
-    extern hsize_t dataset_increase_step;
-    extern hsize_t initial_dataset_size;
-
-    extern uint16_t rest_port;
     extern int n_io_threads;
-    extern size_t n_slots;
+
+    extern size_t ring_buffer_n_slots;
+    extern uint32_t ring_buffer_read_retry_interval;
 
     extern std::string dataset_name;
+    extern hsize_t dataset_increase_step;
+    extern hsize_t initial_dataset_size;
+    extern H5::PredType dataset_type;
+    extern H5T_order_t dataset_byte_order;
 }
 
 #endif
