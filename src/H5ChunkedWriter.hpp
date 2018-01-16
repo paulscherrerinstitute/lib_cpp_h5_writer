0f7bc841f0cfb3661f529fee6cc398b46055fff8
Andrej Babic <andrej.babic@psi.ch>
2018-01-16 15:55:56 +0100

    Add format writer

--- src/H5ChunkedWriter.hpp
+++ src/H5ChunkedWriter.hpp
@@ -31,12 +31,14 @@ class HDF5ChunkedWriter
     hsize_t prepare_storage_for_frame(size_t frame_index, size_t* frame_shape);
     void create_file(size_t* frame_shape, hsize_t frame_chunk=0);
 
+    void write_format_data(H5::CommonFG& file_node, h5_parent& format_node, std::map<std::string, h5_value>& values);
+
     public:
         HDF5ChunkedWriter(const std::string filename, const std::string dataset_name, hsize_t frames_per_file=0, hsize_t initial_dataset_size=config::initial_dataset_size);
         ~HDF5ChunkedWriter();
         void close_file();
         void write_data(size_t frame_index, size_t* frame_shape, size_t data_bytes_size, char* data);
-        void write_format(h5_base& format_root, std::map<std::string, h5_value>& values);
+        void write_format(h5_group& format_root, std::map<std::string, h5_value>& values);
 };
 
 #endif
