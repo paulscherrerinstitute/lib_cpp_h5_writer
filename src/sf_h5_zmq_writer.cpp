0cf8dc34a70c8cf9ccc2a8aa0232eac88df8ce8e
Andrej Babic <andrej.babic@psi.ch>
2018-02-12 17:46:46 +0100

    Add SF format

--- src/sf_h5_zmq_writer.cpp
+++ src/sf_h5_zmq_writer.cpp
@@ -6,13 +6,13 @@
 #include "ProcessManager.hpp"
 #include "WriterManager.hpp"
 #include "ZmqReceiver.hpp"
-#include "format/NXmxFormat.cpp"
+#include "format/SfFormat.cpp"
 
 int main (int argc, char *argv[])
 {
     if (argc != 6) {
         cout << endl;
-        cout << "Usage: h5_zmq_writer [connection_address] [output_file] [n_frames] [rest_port] [user_id]" << endl;
+        cout << "Usage: sf_h5_zmq_writer [connection_address] [output_file] [n_frames] [rest_port] [user_id]" << endl;
         cout << "\tconnection_address: Address to connect to the stream (PULL). Example: tcp://127.0.0.1:40000" << endl;
         cout << "\toutput_file: Name of the output file." << endl;
         cout << "\tn_frames: Number of images to acquire. 0 for infinity (untill /stop is called)." << endl;
@@ -28,12 +28,12 @@ int main (int argc, char *argv[])
     if (user_id != -1) {
 
         #ifdef DEBUG_OUTPUT
-            cout << "[h5_zmq_writer::main] Setting process uid to " << user_id << endl;
+            cout << "[sf_h5_zmq_writer::main] Setting process uid to " << user_id << endl;
         #endif
 
         if (setuid(user_id)) {
             stringstream error_message;
-            error_message << "[h5_zmq_writer::main] Cannot set user_id to " << user_id << endl;
+            error_message << "[sf_h5_zmq_writer::main] Cannot set user_id to " << user_id << endl;
 
             throw runtime_error(error_message.str());
         }
@@ -42,13 +42,16 @@ int main (int argc, char *argv[])
     int n_frames =  atoi(argv[3]);
     string output_file = string(argv[2]);
 
-    NXmxFormat format;
+    SfFormat format;
     
     WriterManager manager(format.get_input_value_type(), output_file, n_frames);
 
     string connect_address = string(argv[1]);
     int n_io_threads = config::zmq_n_io_threads;
     int receive_timeout = config::zmq_receive_timeout;
+    auto header_values = shared_ptr<unordered_map<string, string>>(new unordered_map<string, string> {
+        {"pulse_id", "uint64"},
+    });
     ZmqReceiver receiver(connect_address, n_io_threads, receive_timeout);
 
     int rest_port = atoi(argv[4]);
