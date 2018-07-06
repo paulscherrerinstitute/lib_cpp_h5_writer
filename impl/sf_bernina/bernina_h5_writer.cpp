e9b754af42a4d72a6ac8bc67ea5b0d3f031f3f7f
Andrej Babic <andrej.babic@psi.ch>
2018-07-06 16:07:30 +0200

    Adjust bernina runner

--- impl/sf_bernina/bernina_h5_writer.cpp
+++ impl/sf_bernina/bernina_h5_writer.cpp
@@ -13,7 +13,7 @@ int main (int argc, char *argv[])
 {
     if (argc != 7) {
         cout << endl;
-        cout << "Usage: sf_cpp_h5_writer [connection_address] [output_file] [n_frames]";
+        cout << "Usage: bernina_h5_writer [connection_address] [output_file] [n_frames]";
         cout << " [rest_port] [user_id] [bsread_address]" << endl;
         cout << "\tconnection_address: Address to connect to the stream (PULL). Example: tcp://127.0.0.1:40000" << endl;
         cout << "\toutput_file: Name of the output file." << endl;
