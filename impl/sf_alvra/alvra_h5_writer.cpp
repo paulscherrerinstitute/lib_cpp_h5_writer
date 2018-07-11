b6dc9838f5b7c0e521a318830e14f34d01a29c28
Andrej Babic <andrej.babic@psi.ch>
2018-07-11 13:57:59 +0200

    Add n_modules parameters to Alvra

--- impl/sf_alvra/alvra_h5_writer.cpp
+++ impl/sf_alvra/alvra_h5_writer.cpp
@@ -14,13 +14,14 @@ int main (int argc, char *argv[])
     if (argc != 7) {
         cout << endl;
         cout << "Usage: alvra_h5_writer [connection_address] [output_file] [n_frames]";
-        cout << " [rest_port] [user_id] [bsread_address]" << endl;
+        cout << " [rest_port] [user_id] [bsread_address] [n_modules]" << endl;
         cout << "\tconnection_address: Address to connect to the stream (PULL). Example: tcp://127.0.0.1:40000" << endl;
         cout << "\toutput_file: Name of the output file." << endl;
         cout << "\tn_frames: Number of images to acquire. 0 for infinity (until /stop is called)." << endl;
         cout << "\trest_port: Port to start the REST Api on." << endl;
         cout << "\tuser_id: uid under which to run the writer. -1 to leave it as it is." << endl;
         cout << "\tbsread_address: HTTP address of the bsread REST api." << endl;
+        cout << "\tn_modules: Number of detector modules to be written." << endl;
         cout << endl;
 
         exit(-1);
@@ -32,6 +33,7 @@ int main (int argc, char *argv[])
     int rest_port = atoi(argv[4]);
     int user_id = atoi(argv[5]);
     string bsread_rest_address = string(argv[6]);
+    int n_modules = atoi(argv[7]);
 
     if (user_id != -1) {
         writer_utils::set_process_id(user_id);
@@ -48,17 +50,17 @@ int main (int argc, char *argv[])
         {"is_good_frame", HeaderDataType("uint64")},
         {"daq_rec", HeaderDataType("int64")},
 
-        {"pulse_id_diff", HeaderDataType("int64", 9)},
-        {"framenum_diff", HeaderDataType("int64", 9)},
+        {"pulse_id_diff", HeaderDataType("int64", n_modules)},
+        {"framenum_diff", HeaderDataType("int64", n_modules)},
 
-        {"missing_packets_1", HeaderDataType("uint64", 9)},
-        {"missing_packets_2", HeaderDataType("uint64", 9)},
-        {"daq_recs", HeaderDataType("uint64", 9)},
+        {"missing_packets_1", HeaderDataType("uint64", n_modules)},
+        {"missing_packets_2", HeaderDataType("uint64", n_modules)},
+        {"daq_recs", HeaderDataType("uint64", n_modules)},
         
-        {"pulse_ids", HeaderDataType("uint64", 9)},
-        {"framenums", HeaderDataType("uint64", 9)},
+        {"pulse_ids", HeaderDataType("uint64", n_modules)},
+        {"framenums", HeaderDataType("uint64", n_modules)},
         
-        {"module_number", HeaderDataType("uint64", 9)}
+        {"module_number", HeaderDataType("uint64", n_modules)}
     });
     
     ZmqReceiver receiver(connect_address, config::zmq_n_io_threads, config::zmq_receive_timeout, header_values);
