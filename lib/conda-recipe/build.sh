d3df85fb837c1eedd5ef2a6757ea4f0e432e47c7
Andrej Babic <andrej.babic@psi.ch>
2018-07-12 14:18:50 +0200

    Add header deployment for conda

--- lib/conda-recipe/build.sh
+++ lib/conda-recipe/build.sh
@@ -1,6 +1,7 @@
-make $1
+make
 
 cp bin/* ${PREFIX}/lib
 
 mkdir -p ${PREFIX}/include/cpp_h5_writer
 cp src/*.hpp ${PREFIX}/include/cpp_h5_writer
+cp include/date.h ${CONDA_PREFIX}/include/cpp_h5_writer
