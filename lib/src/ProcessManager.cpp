a37266de2c7fdaaa3dc1a4de59a17372c396f6fe
Andrej Babic <andrej.babic@psi.ch>
2018-11-22 15:30:30 +0100

    Pass n_frames to H5Writer

--- lib/src/ProcessManager.cpp
+++ lib/src/ProcessManager.cpp
@@ -149,7 +149,9 @@ void ProcessManager::receive_zmq()
 
 void ProcessManager::write_h5()
 {
-    auto writer = get_h5_writer(writer_manager.get_output_file(), frames_per_file, config::initial_dataset_size, config::dataset_increase_step);
+    auto writer = get_h5_writer(writer_manager.get_output_file(), writer_manager.get_n_frames(), frames_per_file,
+        config::initial_dataset_size, config::dataset_increase_step);
+        
     auto raw_frames_dataset_name = config::raw_image_dataset_name;
 
     uint64_t last_pulse_id = 0;
