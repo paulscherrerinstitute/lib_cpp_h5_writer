dfe814bfd1e4fd5b8927399ec268ce3d42eb2766
Andrej Babic <andrej.babic@psi.ch>
2018-11-22 16:26:42 +0100

    Remove total frames from H5Writer

--- lib/src/H5Writer.hpp
+++ lib/src/H5Writer.hpp
@@ -12,7 +12,6 @@ class H5Writer
 {
     // Initialized in constructor.
     const std::string filename;
-    hsize_t total_frames;
     hsize_t frames_per_file;
     hsize_t initial_dataset_size;
     hsize_t dataset_increase_step = 0;
@@ -34,7 +33,7 @@ class H5Writer
         const std::string& data_type, const std::string& endianness);
 
     public:
-        H5Writer(const std::string& filename, hsize_t total_frames, hsize_t frames_per_file=0, hsize_t initial_dataset_size=1000, hsize_t dataset_increase_step=1000);
+        H5Writer(const std::string& filename hsize_t frames_per_file=0, hsize_t initial_dataset_size=1000, hsize_t dataset_increase_step=1000);
         virtual ~H5Writer();
         virtual bool is_file_open() const;
         virtual void close_file();
