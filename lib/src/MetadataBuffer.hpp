0281dedec77d010320aabf74520fd9853eb96d3d
Andrej Babic <andrej.babic@psi.ch>
2018-12-03 18:07:50 +0100

    Fix indentation

--- lib/src/MetadataBuffer.hpp
+++ lib/src/MetadataBuffer.hpp
@@ -8,14 +8,14 @@
 
 class MetadataBuffer
 {
-    const uint64_t n_images;
-    const std::shared_ptr<std::unordered_map<std::string, HeaderDataType>> header_values_type;
+	const uint64_t n_images;
+	const std::shared_ptr<std::unordered_map<std::string, HeaderDataType>> header_values_type;
 
-    protected:
+	protected:
 		std::unordered_map<std::string, std::shared_ptr<char>> metadata_buffer;
 		std::unordered_map<std::string, size_t> metadata_length_bytes;
 
-    public:
+	public:
 		MetadataBuffer(uint64_t n_images, std::shared_ptr<std::unordered_map<std::string, HeaderDataType>> header_values_type);
 		void add_metadata_to_buffer(std::string name, uint64_t frame_index, const char* data);
 		std::shared_ptr<char> get_metadata_values(std::string name);
