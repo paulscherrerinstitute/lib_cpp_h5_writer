2ab625c1d750086f9b33dea499d346aaa4009984
Andrej Babic <andrej.babic@psi.ch>
2018-12-05 14:12:09 +0100

    No chunked dataset writing

--- lib/src/BufferedWriter.cpp
+++ lib/src/BufferedWriter.cpp
@@ -1,4 +1,6 @@
+#include "H5Format.hpp"
 #include "BufferedWriter.hpp"
+
 
 using namespace std;
 
@@ -31,8 +33,18 @@ void BufferedWriter::write_metadata_to_file()
 
     if (header_values_type) {
         for (const auto& header_type : *header_values_type) {
-            auto& name = header_type.first;
+            auto& dataset_name = header_type.first;
             auto& header_data_type = header_type.second;
+
+            vector<size_t> data_shape = {header_data_type.value_shape};
+
+            create_dataset(dataset_name, data_shape, header_data_type.type, header_data_type.endianness, false);
+
+            H5::AtomType dataset_data_type(H5FormatUtils::get_dataset_data_type(header_data_type.type));
+            dataset_data_type.setOrder(H5T_ORDER_LE);
+
+            auto& dataset = datasets.at(dataset_name);
+            dataset.write(metadata_buffer->get_metadata_values(dataset_name).get(), dataset_data_type);
         }
     }
 }
