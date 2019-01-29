594931d39a9d2dc7cf7a2dcd17598db2e8a77901
Andrej Babic <andrej.babic@psi.ch>
2019-01-29 17:17:06 +0100

    Remove whitespace

--- lib/src/BufferedWriter.hpp
+++ lib/src/BufferedWriter.hpp
@@ -9,8 +9,6 @@ class BufferedWriter : public H5Writer
     size_t total_frames;
     std::unique_ptr<MetadataBuffer> metadata_buffer;
 
-    
-
     public:
         BufferedWriter(const std::string& filename, size_t total_frames, std::unique_ptr<MetadataBuffer>&& metadata_buffer, 
             hsize_t frames_per_file=0, hsize_t initial_dataset_size=1000, hsize_t dataset_increase_step=1000);
