8b96e28bb3191ee7f30e57ba19248cc9df5aeddf
Babicaa <andrej.babic@psi.ch>
2019-04-25 11:49:35 +0200

    Move writing functions to PSIWriter

--- lib/src/ProcessManager.hpp
+++ lib/src/ProcessManager.hpp
@@ -19,14 +19,6 @@ class ProcessManager
     const std::string& bsread_rest_address;
     hsize_t frames_per_file;
 
-    void notify_first_pulse_id(uint64_t pulse_id);
-    void notify_last_pulse_id(uint64_t pulse_id);
-    
-    protected:
-        void receive_zmq();
-        void write_h5(std::string output_file, uint64_t n_frames);
-        void write_h5_format(H5::H5File& file);
-
     public:
         ProcessManager(WriterManager& writer_manager, 
                        ZmqReceiver& receiver, 
@@ -37,7 +29,6 @@ class ProcessManager
                        hsize_t frames_per_file=0);
 
         void run_receivers(uint8_t n_receiving_threads);
-        void run_writer(std::string output_file, uint64_t n_frames);
 
 };
 
