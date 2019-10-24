789b77c2cce8972ef9db5f0aab19efe7293e736d
Leonardo Hax Damiani <lhdamiani@gmail.com>
2019-10-24 17:35:51 +0200

    ZmqSender class to send out statistics from the writer library.

---
+++ lib/src/ZmqSender.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "ZmqSender.hpp"
+#include "config.hpp"
+#include "H5Format.hpp"
+
+using namespace std;
+namespace pt = boost::property_tree;
+
+ZmqSender::ZmqSender(const std::string& connect_address, const int n_io_threads, const int receive_timeout) :
+        connect_address(connect_address), n_io_threads(n_io_threads),
+        receive_timeout(receive_timeout), sender(NULL)
+{
+    #ifdef DEBUG_OUTPUT
+        using namespace date;
+        cout << "[" << std::chrono::system_clock::now() << "]";
+        cout << "[ZmqSender::ZmqSender] Creating ZMQ sender with";
+        cout << " connect_address " << connect_address;
+        cout << " n_io_threads " << n_io_threads;
+        cout << " receive_timeout " << receive_timeout;
+        cout << endl;
+    #endif
+}
+
+void ZmqSender::bind()
+{
+    #ifdef DEBUG_OUTPUT
+        using namespace date;
+        cout << "[" << std::chrono::system_clock::now() << "]";
+        cout << "[ZmqSender::bind] Binding to address " << connect_address;
+        cout << " with n_io_threads " << n_io_threads << endl;
+    #endif
+
+    context = make_shared<zmq::context_t>(n_io_threads);
+    sender = make_shared<zmq::socket_t>(*context, ZMQ_PUB);
+
+    sender->bind(connect_address);
+    //  Ensure subscriber connection has time to complete
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+}
+
+bool ZmqSender::get_stat(){
+    return stat;
+}
+
+std::string ZmqSender::get_mode(){
+    return mode;
+}
+
+void ZmqSender::set_stat_mode(const bool stat_param, const std::string& mode_indication){
+    stat = stat_param;
+    mode = mode_indication;
+}
+
+
+void ZmqSender::send(const std::string& filter, const std::string& message_data)
+{
+    if (!sender) {
+        stringstream error_message;
+        using namespace date;
+        error_message << "[" << std::chrono::system_clock::now() << "]";
+        error_message << "[ZmqSender::send] Cannot send before connecting. ";
+        error_message << "Connect first." << endl;
+
+        throw runtime_error(error_message.str());
+    }
+    
+    // Send the message 
+    // rv is the return value
+    zmq::message_t message_filter(filter.size());
+    memcpy (message_filter.data(), filter.data(), filter.size());
+    zmq::message_t message_content(message_data.size());
+    memcpy (message_content.data(), message_data.data(), message_data.size());
+    
+    auto rv0 = sender->send(message_filter, ZMQ_SNDMORE);
+    auto rv1 = sender->send(message_content);
+
+    // verifies the return value
+    if (rv0 != 0 && rv1 != 0) {
+        using namespace date;
+        cout << "[" << std::chrono::system_clock::now() << "]";
+        cout << "[ZmqSender::send] Error while sending statistics via ZMQ."; 
+    }
+}
---
+++ lib/src/ZmqSender.hpp
@@ -0,0 +1,46 @@
+#ifndef ZMQSENDER_H
+#define ZMQSENDER_H
+
+#include <string>
+#include <memory>
+#include <tuple>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+#include <boost/property_tree/json_parser.hpp>
+#include <zmq.hpp>
+#include <thread>
+#include <chrono>
+
+
+class ZmqSender
+{
+    const std::string connect_address;
+    const int receive_timeout;
+    std::string filter;
+    bool stat;
+    const int n_io_threads;
+    std::string mode;
+
+    std::shared_ptr<zmq::socket_t> sender = NULL;
+    std::shared_ptr<zmq::context_t> context = NULL;
+
+
+    public:
+    ZmqSender(const std::string& connect_address, 
+                const int n_io_threads, const int receive_timeout);
+    virtual ~ZmqSender(){};
+
+    void bind();
+
+    void send(const std::string& filter, const std::string& message_data);
+
+    void set_stat_mode(bool stat, const std::string& mode_indication);
+
+    bool get_stat();
+
+    std::string get_mode();
+
+};
+
+#endif
