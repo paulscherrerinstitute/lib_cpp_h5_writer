4d3ac76bbb1540c9a491068591f8cbedfec0b437
Andrej Babic <andrej.babic@psi.ch>
2020-03-26 14:48:24 +0100

    Cosmetic fixes in header

--- core-writer/src/ZmqReceiver.hpp
+++ core-writer/src/ZmqReceiver.hpp
@@ -28,11 +28,15 @@ struct HeaderDataType
 
 size_t get_type_byte_size(const std::string& type);
 
-void copy_value_to_buffer(const char* buffer, size_t offset, const boost::property_tree::ptree& json_value, 
-    const HeaderDataType& header_data_type);
+void copy_value_to_buffer(
+        const char* buffer, size_t offset,
+        const boost::property_tree::ptree& json_value,
+        const HeaderDataType& header_data_type);
 
-std::shared_ptr<char> get_value_from_json(const boost::property_tree::ptree& json_header, 
-    const std::string& name, const HeaderDataType& header_data_type);
+std::shared_ptr<char> get_value_from_json(
+        const boost::property_tree::ptree& json_header,
+        const std::string& name,
+        const HeaderDataType& header_data_type);
 
 class ZmqReceiver
 {
