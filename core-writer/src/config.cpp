f4e7d9505eb24e253b49e84ccfaad4fb2b66acd9
Andrej Babic <andrej.babic@psi.ch>
2020-04-06 13:33:45 +0200

    Remove default value for user_id of writing process
    Does not really make sense since the value is hard-coded anyway and will not change
    in different deployments.

--- core-writer/src/config.cpp
+++ core-writer/src/config.cpp
@@ -22,7 +22,4 @@ namespace config {
     hsize_t dataset_increase_step = 1000;
     // To which value to initialize a dataset size.
     hsize_t initial_dataset_size = 1000;
-
-    // -1 means to stay with the same user_id.
-    int default_writing_user_id = -1;
 }
