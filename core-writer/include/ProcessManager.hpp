093271afeb86f8fb7bb8cb64bdc559c2df847add
Andrej Babic <andrej.babic@psi.ch>
2020-04-07 12:20:10 +0200

    Define ProcessManager interface

--- core-writer/include/ProcessManager.hpp
+++ core-writer/include/ProcessManager.hpp
@@ -1,12 +1,6 @@
 #ifndef PROCESSMANAGER_H
 #define PROCESSMANAGER_H
 
-#include "WriterUtils.hpp"
-#include "H5Format.hpp"
-#include "RingBuffer.hpp"
-#include "ZmqReceiver.hpp"
-#include <chrono>
-#include "date.h"
 #include "H5WriteModule.hpp"
 #include "ZmqRecvModule.hpp"
 
@@ -21,8 +15,20 @@ class ProcessManager
 
         void start_rest_api(const uint16_t rest_port);
 
+        void start_writing(
+                const std::string output_file,
+                const int n_frames,
+                const int user_id);
+        void stop_writing();
+
+        void start_receiving(
+                const std::string connect_address,
+                const int n_receiving_threads);
+        void stop_receiving();
 
         std::string get_status();
+
+        std::unordered_map<std::string, float> get_statistics();
 
 };
 
