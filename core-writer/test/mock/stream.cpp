2329f7ad2fc6e769d55f3c200ee7f7c9782ada88
Andrej Babic <andrej.babic@psi.ch>
2020-04-08 16:02:03 +0200

    Make stream generation a generic mock

---
+++ core-writer/test/mock/stream.cpp
@@ -0,0 +1,20 @@
+#include <zmq.hpp>
+
+void generate_stream(size_t n_messages)
+{
+    zmq::context_t context(1);
+    zmq::socket_t socket(context, ZMQ_PUSH);
+    socket.bind("tcp://127.0.0.1:11000");
+
+    string header = "{\"frame\": 0, \"shape\": [1,16], \"type\": \"uint8\"}";
+    zmq::const_buffer header_msg(header.c_str(), header.length());
+
+    size_t buffer_size = 16;
+    char buffer[buffer_size];
+    zmq::const_buffer buffer_msg(buffer, buffer_size);
+
+    for (size_t i=0; i<n_messages; i++) {
+        socket.send(header_msg, zmq::send_flags::sndmore);
+        socket.send(buffer_msg);
+    }
+}
