72efda070167f239d5646d1c3ad317ea7ee7843c
Andrej Babic <andrej.babic@psi.ch>
2020-04-15 15:06:50 +0200

    Improve UdpRecvModuleTest

--- core-writer/test/test_UdpRecvModule.cpp
+++ core-writer/test/test_UdpRecvModule.cpp
@@ -12,7 +12,7 @@ TEST(UdpRecvModule, basic_interaction)
     RingBuffer<UdpFrameMetadata> ring_buffer(10);
     UdpRecvModule udp_recv_module(ring_buffer);
 
-    udp_recv_module.start_recv(udp_port, JUNGFRAU_BYTES_PER_PACKET);
+    udp_recv_module.start_recv(udp_port, JUNGFRAU_DATA_BYTES_PER_FRAME);
 
     EXPECT_THROW(
             udp_recv_module.start_recv(udp_port, JUNGFRAU_BYTES_PER_PACKET),
@@ -31,7 +31,7 @@ TEST(UdpRecvModule, simple_recv)
     RingBuffer<UdpFrameMetadata> ring_buffer(10);
     UdpRecvModule udp_recv_module(ring_buffer);
 
-    udp_recv_module.start_recv(udp_port, JUNGFRAU_BYTES_PER_PACKET);
+    udp_recv_module.start_recv(udp_port, JUNGFRAU_DATA_BYTES_PER_FRAME);
 
     this_thread::sleep_for(chrono::milliseconds(100));
 
@@ -43,10 +43,12 @@ TEST(UdpRecvModule, simple_recv)
     auto server_address = get_server_address(udp_port);
 
     jungfrau_packet send_udp_buffer;
-    send_udp_buffer.framenum = 1;
     send_udp_buffer.bunchid = 100;
     send_udp_buffer.debug = 1000;
 
+    ASSERT_TRUE(ring_buffer.is_empty());
+
+    send_udp_buffer.framenum = 1;
     for (size_t i=0; i<n_msg; i++) {
         send_udp_buffer.packetnum = i;
 
@@ -59,20 +61,32 @@ TEST(UdpRecvModule, simple_recv)
                 sizeof(server_address));
     }
 
-    ASSERT_TRUE(ring_buffer.is_empty());
+    ASSERT_FALSE(ring_buffer.is_empty());
+    auto result = ring_buffer.read();
+    // The slot should be reserved, but not yet committed.
+    // Only with next frame packet commit.
+    ASSERT_TRUE(result.first == NULL);
 
     // When packet from new frame is received, the previous frame should be
     // committed to the ring buffer.
     send_udp_buffer.framenum = 2;
-    ::sendto(
-            send_socket_fd,
-            &send_udp_buffer,
-            JUNGFRAU_BYTES_PER_PACKET,
-            0,
-            (sockaddr*) &server_address,
-            sizeof(server_address));
+    for (size_t i=0; i<128; i++){
+        send_udp_buffer.packetnum = i;
+
+        ::sendto(
+                send_socket_fd,
+                &send_udp_buffer,
+                JUNGFRAU_BYTES_PER_PACKET,
+                0,
+                (sockaddr*) &server_address,
+                sizeof(server_address));
+    }
+
+    this_thread::sleep_for(chrono::milliseconds(100));
 
     ASSERT_FALSE(ring_buffer.is_empty());
+    auto result2 = ring_buffer.read();
+    ASSERT_TRUE(result2.first != NULL);
 
     ::close(send_socket_fd);
 }
