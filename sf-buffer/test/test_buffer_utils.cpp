8c5086b15b043536a5e00a8c2de95fb7a9ab7cb6
Andrej Babic <andrej.babic@psi.ch>
2020-04-15 16:17:53 +0200

    Add buffer util tests

---
+++ sf-buffer/test/test_buffer_utils.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "../src/buffer_utils.hpp"
+
+using namespace std;
+
+TEST(BufferUtils, folder_test)
+{
+
+}
---
+++ sf-buffer/test/test_main.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "test_buffer_utils.cpp"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
