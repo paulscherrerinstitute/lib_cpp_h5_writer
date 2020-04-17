b5ed279f4bedc24e79c31b94ce19cb599211f70d
Andrej Babic <andrej.babic@psi.ch>
2020-04-17 11:16:26 +0200

    Add format marker to JF file format

--- core-buffer/include/JFFileFormat.hpp
+++ core-buffer/include/JFFileFormat.hpp
@@ -3,9 +3,12 @@
 
 #include "jungfrau.hpp"
 
+const char JF_FORMAT_START_BYTE = 0xBE;
+
 #pragma pack(push)
 #pragma pack(1)
 struct JFFileFormat {
+    const char FORMAT_MARKER = JF_FORMAT_START_BYTE;
     uint64_t pulse_id;
     uint64_t frame_id;
     uint32_t daq_rec;
