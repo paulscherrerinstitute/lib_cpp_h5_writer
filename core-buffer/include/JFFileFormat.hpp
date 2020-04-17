201d18ac0e8b5e7c421799ab323fde46012cbbf4
Andrej Babic <andrej.babic@psi.ch>
2020-04-17 10:02:41 +0200

    Add serial file format for JF

---
+++ core-buffer/include/JFFileFormat.hpp
@@ -0,0 +1,15 @@
+#ifndef JFFILEFORMAT_HPP
+#define JFFILEFORMAT_HPP
+
+#pragma pack(push)
+#pragma pack(1)
+struct JFFileFormat {
+    uint64_t pulse_id;
+    uint64_t frame_id;
+    uint32_t daq_rec;
+    uint16 n_recv_packets;
+    char* data[JUNGFRAU_DATA_BYTES_PER_FRAME];
+};
+#pragma pack(pop)
+
+#endif // JFFILEFORMAT_HPP
