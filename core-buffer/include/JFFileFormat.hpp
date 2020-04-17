6cf70867616d482ba2372bc28067a8432c2cc033
Andrej Babic <andrej.babic@psi.ch>
2020-04-17 11:42:11 +0200

    Add constructor to JF struct

--- core-buffer/include/JFFileFormat.hpp
+++ core-buffer/include/JFFileFormat.hpp
@@ -8,7 +8,10 @@ const char JF_FORMAT_START_BYTE = 0xBE;
 #pragma pack(push)
 #pragma pack(1)
 struct JFFileFormat {
-    const char FORMAT_MARKER = JF_FORMAT_START_BYTE;
+
+    JFFileFormat() : FORMAT_MARKER(JF_FORMAT_START_BYTE) {};
+
+    const char FORMAT_MARKER;
     uint64_t pulse_id;
     uint64_t frame_id;
     uint32_t daq_rec;
