57b0cdba07f00ff93a528d82d77200ba9e44aaf2
Andrej Babic <andrej.babic@psi.ch>
2020-04-17 11:37:03 +0200

    Add BinaryWriter test

--- core-buffer/test/test_main.cpp
+++ core-buffer/test/test_main.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "test_UdpReceiver.cpp"
 #include "test_UdpRecvModule.cpp"
+#include "test_BinaryWriter.cpp"
 #include "test_buffer_utils.cpp"
 
 using namespace std;
