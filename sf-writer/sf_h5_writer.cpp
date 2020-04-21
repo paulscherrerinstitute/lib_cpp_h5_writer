8783ca8d877946f3bbeb0e98eb3b303633ad0d1b
Andrej Babic <andrej.babic@psi.ch>
2020-04-21 12:18:28 +0200

    Remove format reminder

--- sf-writer/sf_h5_writer.cpp
+++ sf-writer/sf_h5_writer.cpp
@@ -75,22 +75,3 @@ int main (int argc, char *argv[])
 
     return 0;
 }
-
-//unordered_map<string, HeaderDataType> header_values {
-//            {"pulse_id", HeaderDataType("uint64")},
-//            {"frame", HeaderDataType("uint64")},
-//            {"is_good_frame", HeaderDataType("uint64")},
-//            {"daq_rec", HeaderDataType("int64")},
-//
-//            {"pulse_id_diff", HeaderDataType("int64", n_modules)},
-//            {"framenum_diff", HeaderDataType("int64", n_modules)},
-//
-//            {"missing_packets_1", HeaderDataType("uint64", n_modules)},
-//            {"missing_packets_2", HeaderDataType("uint64", n_modules)},
-//            {"daq_recs", HeaderDataType("uint64", n_modules)},
-//
-//            {"pulse_ids", HeaderDataType("uint64", n_modules)},
-//            {"framenums", HeaderDataType("uint64", n_modules)},
-//
-//            {"module_number", HeaderDataType("uint64", n_modules)}
-//    });
