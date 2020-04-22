ac5624cafcb91df124048c88bf29838c1b594103
Andrej Babic <andrej.babic@psi.ch>
2020-04-22 19:22:17 +0200

    Made smaller batches for writing

--- core-buffer/include/BufferUtils.hpp
+++ core-buffer/include/BufferUtils.hpp
@@ -6,7 +6,7 @@
 
 namespace BufferUtils
 {
-    const size_t STREAM_BLOCK_SIZE = 1000;
+    const size_t STREAM_BLOCK_SIZE = 100;
     extern const size_t FILE_MOD;
     extern const size_t FOLDER_MOD;
     extern const std::string FILE_EXTENSION;
--- sf-buffer/src/sf_replay.cpp
+++ sf-buffer/src/sf_replay.cpp
@@ -6,6 +6,58 @@
 #include "zmq.h"
 
 using namespace std;
+using namespace BufferUtils;
+
+void load_data_from_file (
+        BufferUtils::FileBufferMetadata* metadata_buffer,
+        char* image_buffer,
+        const string &filename,
+        const size_t start_index)
+{
+
+    hsize_t image_dim[3] = {BufferUtils::FILE_MOD, 512, 1024};
+    H5::DataSpace image_space (3, image_dim);
+    hsize_t i_count[] = {STREAM_BLOCK_SIZE, 512, 1024};
+    hsize_t i_start[] = {start_index, 0, 0};
+    image_space.selectHyperslab(H5S_SELECT_SET, i_count, i_start);
+
+    hsize_t metadata_dim[2] = {BufferUtils::FILE_MOD, 1};
+    H5::DataSpace metadata_space (2, metadata_dim);
+    hsize_t m_count[] = {STREAM_BLOCK_SIZE, 1};
+    hsize_t m_start[] = {start_index, 0};
+    metadata_space.selectHyperslab(H5S_SELECT_SET, m_count, m_start);
+
+    H5::H5File input_file(filename, H5F_ACC_RDONLY);
+
+    auto image_dataset = input_file.openDataSet("image");
+    image_dataset.read(
+            image_buffer, H5::PredType::NATIVE_UINT16,
+            H5::DataSpace::ALL, image_space);
+
+    auto pulse_id_dataset = input_file.openDataSet("pulse_id");
+    pulse_id_dataset.read(
+            metadata_buffer->pulse_id, H5::PredType::NATIVE_UINT64,
+            H5::DataSpace::ALL, metadata_space);
+
+    auto frame_id_dataset = input_file.openDataSet("frame_id");
+    frame_id_dataset.read(
+            metadata_buffer->frame_index, H5::PredType::NATIVE_UINT64,
+            H5::DataSpace::ALL, metadata_space);
+
+    auto daq_rec_dataset = input_file.openDataSet("daq_rec");
+    daq_rec_dataset.read(
+            metadata_buffer->daq_rec, H5::PredType::NATIVE_UINT32,
+            H5::DataSpace::ALL, metadata_space);
+
+    auto received_packets_dataset =
+            input_file.openDataSet("received_packets");
+    received_packets_dataset.read(
+            metadata_buffer->n_received_packets,
+            H5::PredType::NATIVE_UINT16,
+            H5::DataSpace::ALL, metadata_space);
+
+    input_file.close();
+}
 
 int main (int argc, char *argv[]) {
 
@@ -30,19 +82,17 @@ int main (int argc, char *argv[]) {
     uint64_t start_pulse_id = (uint64_t) atoll(argv[4]);
     uint64_t stop_pulse_id = (uint64_t) atoll(argv[5]);
 
-    auto metadata_buffer = make_unique<BufferUtils::FileBufferMetadata>();
+    auto metadata_buffer = make_unique<FileBufferMetadata>();
     metadata_buffer->module_id = module_id;
 
-    auto image_buffer = make_unique<uint16_t[]>(
-            BufferUtils::STREAM_BLOCK_SIZE * 512 * 1024);
+    auto image_buffer = make_unique<uint16_t[]>(STREAM_BLOCK_SIZE * 512 * 1024);
 
-    auto path_suffixes = BufferUtils::get_path_suffixes(
-            start_pulse_id, stop_pulse_id);
+    auto path_suffixes = get_path_suffixes(start_pulse_id, stop_pulse_id);
 
     auto ctx = zmq_ctx_new();
 
     auto socket = zmq_socket(ctx, ZMQ_PUSH);
-    int sndhwm = 10;
+    int sndhwm = STREAM_BLOCK_SIZE;
     if (zmq_setsockopt(socket, ZMQ_SNDHWM, &sndhwm, sizeof(sndhwm)) != 0) {
         throw runtime_error(strerror (errno));
     };
@@ -68,53 +118,37 @@ int main (int argc, char *argv[]) {
                 channel_name + "/" +
                 suffix.path;
 
-        cout << "Reading file " << filename << endl;
+        for (
+                size_t i_batch=0;
+                i_batch<FILE_MOD;
+                i_batch+STREAM_BLOCK_SIZE)
+        {
+            load_data_from_file(
+                    metadata_buffer.get(),
+                    (char*)(image_buffer.get()),
+                    filename,
+                    i_batch);
 
-        H5::H5File input_file(filename, H5F_ACC_RDONLY);
+            for (size_t i_frame=0; i_frame < FILE_MOD; i_frame++) {
 
-        auto image_dataset = input_file.openDataSet("image");
-        image_dataset.read(
-                image_buffer.get(), H5::PredType::NATIVE_UINT16);
+                ModuleFrame module_frame = {
+                        metadata_buffer->pulse_id[i_frame],
+                        metadata_buffer->frame_index[i_frame],
+                        metadata_buffer->daq_rec[i_frame],
+                        metadata_buffer->n_received_packets[i_frame],
+                        module_id
+                };
 
-        auto pulse_id_dataset = input_file.openDataSet("pulse_id");
-        pulse_id_dataset.read(
-                metadata_buffer->pulse_id, H5::PredType::NATIVE_UINT64);
+                zmq_send(socket,
+                         &module_frame,
+                         sizeof(ModuleFrame),
+                         ZMQ_SNDMORE);
 
-        auto frame_id_dataset = input_file.openDataSet("frame_id");
-        frame_id_dataset.read(
-                metadata_buffer->frame_index, H5::PredType::NATIVE_UINT64);
-
-        auto daq_rec_dataset = input_file.openDataSet("daq_rec");
-        daq_rec_dataset.read(
-                metadata_buffer->daq_rec, H5::PredType::NATIVE_UINT32);
-
-        auto received_packets_dataset =
-                input_file.openDataSet("received_packets");
-        received_packets_dataset.read(
-                metadata_buffer->n_received_packets,
-                H5::PredType::NATIVE_UINT16);
-
-        input_file.close();
-
-        for (size_t i_frame=0; i_frame < BufferUtils::FILE_MOD; i_frame++) {
-
-            ModuleFrame module_frame = {
-                    metadata_buffer->pulse_id[i_frame],
-                    metadata_buffer->frame_index[i_frame],
-                    metadata_buffer->daq_rec[i_frame],
-                    metadata_buffer->n_received_packets[i_frame],
-                    module_id
-            };
-
-            zmq_send(socket,
-                     &module_frame,
-                     sizeof(ModuleFrame),
-                     ZMQ_SNDMORE);
-
-            zmq_send(socket,
-                     (char*) (image_buffer.get() + (i_frame * 512 * 1024)),
-                     512 * 1024 * 2,
-                     0);
+                zmq_send(socket,
+                         (char*) (image_buffer.get() + (i_frame * 512 * 1024)),
+                         512 * 1024 * 2,
+                         0);
+            }
         }
     }
 
