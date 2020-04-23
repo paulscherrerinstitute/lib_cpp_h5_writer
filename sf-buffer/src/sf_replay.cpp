1d517f1ae2a460a7b547f03a45be014477ea2f35
Andrej Babic <andrej.babic@psi.ch>
2020-04-23 18:25:01 +0200

    Make buffer structure explicit

--- sf-buffer/src/sf_replay.cpp
+++ sf-buffer/src/sf_replay.cpp
@@ -23,46 +23,57 @@ void load_data_from_file (
         const size_t start_index)
 {
 
-    hsize_t image_dim[3] = {FILE_MOD, 512, 1024};
-    H5::DataSpace image_space (3, image_dim);
-    hsize_t i_count[] = {REPLAY_BLOCK_SIZE, 512, 1024};
-    hsize_t i_start[] = {start_index, 0, 0};
-    image_space.selectHyperslab(H5S_SELECT_SET, i_count, i_start);
+    hsize_t b_image_dim[3] = {REPLAY_BLOCK_SIZE, 512, 1024};
+    H5::DataSpace b_i_space (3, b_image_dim);
+    hsize_t b_i_count[] = {REPLAY_BLOCK_SIZE, 512, 1024};
+    hsize_t b_i_start[] = {0, 0, 0};
+    b_i_space.selectHyperslab(H5S_SELECT_SET, b_i_count, b_i_start);
 
-    hsize_t metadata_dim[2] = {FILE_MOD, 1};
-    H5::DataSpace metadata_space (2, metadata_dim);
-    hsize_t m_count[] = {REPLAY_BLOCK_SIZE, 1};
-    hsize_t m_start[] = {start_index, 0};
-    metadata_space.selectHyperslab(H5S_SELECT_SET, m_count, m_start);
+    hsize_t f_image_dim[3] = {FILE_MOD, 512, 1024};
+    H5::DataSpace f_i_space (3, f_image_dim);
+    hsize_t f_i_count[] = {REPLAY_BLOCK_SIZE, 512, 1024};
+    hsize_t f_i_start[] = {start_index, 0, 0};
+    f_i_space.selectHyperslab(H5S_SELECT_SET, f_i_count, f_i_start);
+
+    hsize_t b_metadata_dim[2] = {REPLAY_BLOCK_SIZE, 1};
+    H5::DataSpace b_m_space (2, b_metadata_dim);
+    hsize_t b_m_count[] = {REPLAY_BLOCK_SIZE, 1};
+    hsize_t b_m_start[] = {0, 0};
+    b_m_space.selectHyperslab(H5S_SELECT_SET, b_m_count, b_m_start);
+
+    hsize_t f_metadata_dim[2] = {FILE_MOD, 1};
+    H5::DataSpace f_m_space (2, f_metadata_dim);
+    hsize_t f_m_count[] = {REPLAY_BLOCK_SIZE, 1};
+    hsize_t f_m_start[] = {start_index, 0};
+    f_m_space.selectHyperslab(H5S_SELECT_SET, f_m_count, f_m_start);
 
     H5::H5File input_file(filename, H5F_ACC_RDONLY);
 
     auto image_dataset = input_file.openDataSet("image");
     image_dataset.read(
             image_buffer, H5::PredType::NATIVE_UINT16,
-            H5::DataSpace::ALL, image_space);
+            b_i_space, f_i_space);
 
     auto pulse_id_dataset = input_file.openDataSet("pulse_id");
     pulse_id_dataset.read(
             metadata_buffer->pulse_id, H5::PredType::NATIVE_UINT64,
-            H5::DataSpace::ALL, metadata_space);
+            b_m_space, f_m_space);
 
     auto frame_id_dataset = input_file.openDataSet("frame_id");
     frame_id_dataset.read(
             metadata_buffer->frame_index, H5::PredType::NATIVE_UINT64,
-            H5::DataSpace::ALL, metadata_space);
+            b_m_space, f_m_space);
 
     auto daq_rec_dataset = input_file.openDataSet("daq_rec");
     daq_rec_dataset.read(
             metadata_buffer->daq_rec, H5::PredType::NATIVE_UINT32,
-            H5::DataSpace::ALL, metadata_space);
+            b_m_space, f_m_space);
 
     auto received_packets_dataset =
             input_file.openDataSet("received_packets");
     received_packets_dataset.read(
-            metadata_buffer->n_received_packets,
-            H5::PredType::NATIVE_UINT16,
-            H5::DataSpace::ALL, metadata_space);
+            metadata_buffer->n_received_packets, H5::PredType::NATIVE_UINT16,
+            b_m_space, f_m_space);
 
     input_file.close();
 }
