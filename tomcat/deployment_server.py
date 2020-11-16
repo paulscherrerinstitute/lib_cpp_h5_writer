6a2144a3a8e1c676a91b19b02aeadaff877db869
Leonardo Hax Damiani <leonardo.hax@psi.ch>
2020-11-16 18:44:07 +0100

    Updated deployment server for multi camera usage.

--- tomcat/deployment_server.py
+++ tomcat/deployment_server.py
@@ -1,5 +1,26 @@
 #!/bin/env python
 from waitress import serve
 import start_server
+import sys
 from paste.translogger import TransLogger
-serve(TransLogger(start_server.app, setup_console_handler=False), port=9901)
+from enum import Enum
+
+class Cam(Enum):
+    PCO3 = "PCO3", 9902
+    PCO4 = "PCO4", 9901
+
+args = [str(arg.upper()) for arg in sys.argv]
+cam_arg = None
+
+if len(args) != 2 and (Cam.PCO3.value[0] != args[0] and Cam.PCO4.value[0] != args[0]):
+    print("\nPCO DAQ Server Error: PCO Camera not specified/recognized. Valid camera names: \"PCO3\" or \"PCO4\".\n")
+    raise SystemExit("Usage: %s <PCO3/PCO4>" % sys.argv[0])
+elif args[1] == Cam.PCO3.value[0]:
+    cam_arg = Cam.PCO3
+elif args[1] == Cam.PCO4.value[0]:
+    cam_arg = Cam.PCO4
+else:
+    raise SystemExit("Usage: %s <PCO3/PCO4>" % sys.argv[0])
+
+if cam_arg not None:
+    serve(TransLogger(start_server.app, setup_console_handler=False), port=cam_arg.value[1])
